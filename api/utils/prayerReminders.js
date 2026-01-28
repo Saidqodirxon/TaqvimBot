12ba56819a76a47b5cd48a9653de4ad3464ae1e1
Saidqodirxon Rahimov <rsaidqodirxon@gmail.com>
2026-01-28 12:39:55 +0500

    Fix syntax error: remove trailing commas in inline_keyboard

--- api/utils/prayerReminders.js
+++ api/utils/prayerReminders.js
@@ -107,7 +107,7 @@ async function schedulePrayerReminders(bot, user) {
                           callback_data: "disable_all_reminders",
                         },
                       ],
-                    ],
+                    ]
                   },
                 });
               } catch (error) {
@@ -139,7 +139,7 @@ async function schedulePrayerReminders(bot, user) {
                         callback_data: "disable_all_reminders",
                       },
                     ],
-                  },
+                  ],
                 },
               });
             } catch (error) {
